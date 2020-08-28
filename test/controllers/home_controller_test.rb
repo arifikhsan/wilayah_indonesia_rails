2fb9bb301be6683f850c215ade943ae6e568f809
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-28 08:11:52 +0700

    test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get home_index_url
+  test "should get root" do
+    get root_url
     assert_response :success
   end
-
 end
