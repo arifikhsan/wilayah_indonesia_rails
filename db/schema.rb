32a38d117e7123727b86f0e9cf37036b17da61ee
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-28 07:56:36 +0700

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_003140) do
+ActiveRecord::Schema.define(version: 2020_08_28_005223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "districts", force: :cascade do |t|
+    t.bigint "regency_id", null: false
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["regency_id"], name: "index_districts_on_regency_id"
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "regencies", force: :cascade do |t|
+    t.bigint "province_id", null: false
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["province_id"], name: "index_regencies_on_province_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,4 +64,15 @@ ActiveRecord::Schema.define(version: 2020_08_28_003140) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  create_table "villages", force: :cascade do |t|
+    t.bigint "district_id", null: false
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["district_id"], name: "index_villages_on_district_id"
+  end
+
+  add_foreign_key "districts", "regencies"
+  add_foreign_key "regencies", "provinces"
+  add_foreign_key "villages", "districts"
 end
