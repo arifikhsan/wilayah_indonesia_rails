d1a507314dfb6d9ac2b6437ae44d2fa648fff8a1
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-28 07:25:41 +0700

    change db to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_28_001805) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
